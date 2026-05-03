b515ce994e681f96dd3d10b3c6ee105144834634
harshsahu0030 <harsh0030sahu@gmail.com>
2026-05-04 00:26:07 +0530

    add REST api

--- app/(client)/page.js
+++ app/(client)/page.js
@@ -1,5 +1,19 @@
-const page = () => {
-  return <div>page</div>;
+import { getTreatments } from "@/lib/getTreatments";
+
+const page = async () => {
+  const treatments = await getTreatments();
+
+  return (
+    <div className="flex flex-col gap-5 p-10">
+      {treatments?.map((treatment) => (
+        <div key={treatment?.id} className="flex flex-col gap-2 border-2 p-5">
+          <span>{treatment?.title?.rendered}</span>
+          <span>{treatment?.slug}</span>
+          <span></span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default page;
