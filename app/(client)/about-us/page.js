66188c936bb943b95cd1aac596866620ca9577b0
harshsahu0030 <harsh0030sahu@gmail.com>
2026-05-03 22:58:28 +0530

    created  boilerplate

---
+++ app/(client)/about-us/page.js
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>About Us</div>;
+};
+
+export default page;
---
+++ app/(client)/our-treatment/page.js
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Our Treatments</div>;
+};
+
+export default page;
