bd837e10b4ccbe3727d91a93551b8702426f7019
harshsahu0030 <harsh0030sahu@gmail.com>
2026-05-03 23:01:35 +0530

    fixed language bug

--- app/layout.js
+++ app/layout.js
@@ -7,10 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
