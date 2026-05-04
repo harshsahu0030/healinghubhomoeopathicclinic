2f13f8e9faeec0c71d3885db5515837cbf54c5a1
harshsahu0030 <harsh0030sahu@gmail.com>
2026-05-04 15:55:07 +0530

    add more apis

--- app/(client)/page.js
+++ app/(client)/page.js
@@ -1,19 +1,17 @@
+import { getBlog } from "@/lib/getBlog";
+import { getTestimonials } from "@/lib/getTestimonial";
 import { getTreatments } from "@/lib/getTreatments";
 
 const page = async () => {
   const treatments = await getTreatments();
+  const testimonials = await getTestimonials();
+  const blog = await getBlog();
 
-  return (
-    <div className="flex flex-col gap-5 p-10">
-      {treatments?.map((treatment) => (
-        <div key={treatment?.id} className="flex flex-col gap-2 border-2 p-5">
-          <span>{treatment?.title?.rendered}</span>
-          <span>{treatment?.slug}</span>
-          <span></span>
-        </div>
-      ))}
-    </div>
-  );
+  console.log(treatments);
+  console.log(testimonials);
+  console.log(blog);
+
+  return <div className="flex flex-col gap-5 p-10"></div>;
 };
 
 export default page;
