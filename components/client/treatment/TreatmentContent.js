13dca08dceab7a43915c16ad87c66e82743b3ad1
harshsahu0030 <harsh0030sahu@gmail.com>
2026-05-07 16:21:35 +0530

    again

--- components/client/treatment/TreatmentContent.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-
-const TreatmentContent = ({ treatment = {} }) => {
-  return <div className="flex flex-col gap-5"></div>;
-};
-
-export default TreatmentContent;
