ad83c511d6a9f5f69cfbc66d263bfd970a8937d8
harshsahu0030 <harsh0030sahu@gmail.com>
2026-05-07 16:07:06 +0530

    remove treatment contect component

--- app/(client)/our-treatments/[treatmentSlug]/page.js
+++ app/(client)/our-treatments/[treatmentSlug]/page.js
@@ -1,5 +1,4 @@
 import Heading from "@/components/client/common/Heading";
-import TreatmentContent from "@/components/client/treatment/TreatmentContent";
 import TreatmentList from "@/components/client/treatment/TreatmentList";
 import { getTreatment, getTreatmentsList } from "@/lib/graphql/treatment";
 
@@ -9,17 +8,13 @@ const page = async ({ params }) => {
   const treatment = await getTreatment(treatmentSlug);
   const treatments = await getTreatmentsList();
 
-  console.log(treatment);
-
   return (
     <>
       <Heading title={treatment?.title} type="treatment" />
 
       <section className="padding py-20 h-full w-full grid grid-cols-1 lg:grid-cols-12 gap-5">
         {/* left  */}
-        <div className="w-full lg:col-span-8">
-          <TreatmentContent treatment={treatment} />
-        </div>
+        <div className="w-full lg:col-span-8"></div>
 
         {/* right  */}
         <article className="w-full lg:col-span-4 flex flex-col gap-5">
